13945e2754794e9984f1394db64f0e461e14ce31
Matthew Casperson <matthewcasperson@gmail.com>
2021-03-09 07:53:30 +1000

    Fixed up the test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,8 @@
 describe('Random Quotes', () => {
     it('Can refresh', () => {
-        cy.get('.refreshQuote').click()
+    	cy.visit('/')
+        cy.get('#refreshQuote').click()
         cy.wait(1000)
-        cy.get('.quoteCount').contains('2')
+        cy.get('#quoteText').should('not.be.empty')
     })
 })
